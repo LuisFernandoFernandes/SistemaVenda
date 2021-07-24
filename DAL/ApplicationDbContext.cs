2ef10ee8aa2bb0bd64406e5aab8b9bfd65964e13
LuisFernandoFernandes <luisfernando.fernandes@hotmail.com>
2021-07-24 16:31:29 -0300

    Criando camada de acesso a dados com o Entity Framework Core, com a criação da classe ApplicationDbContext.

---
+++ DAL/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using SistemaVenda.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Entity
+
+namespace SistemaVenda.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Venda> Venda { get; set; }
+        public DbSet<VendaProdutos> VendaProdutos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<VendaProdutos>().HasKey(x => new { x.CodigoVenda, x.CodigoProduto });
+
+            modelBuilder.Entity<VendaProdutos>()
+                 .HasRequired(x => x.Venda)
+                 .WithMany(y => y.Produtos)
+                 .HasForeignKey(x => x.CodigoVenda);
+
+            modelBuilder.Entity<VendaProdutos>()
+                 .HasRequired(x => x.Produto)
+                 .WithMany(y => y.Vendas)
+                 .HasForeignKey(x => x.CodigoProduto);
+
+
+        }
+    }
+}
